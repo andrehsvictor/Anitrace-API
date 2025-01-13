87c79ff44fd5a4a79f0cd63c37eaff71e7f57df8
andrehsvictor <andrehsvictor@gmail.com>
2025-01-13 20:34:23 -0300

    feat: add dependencies for RefreshTokenRepository, RevokedTokenRepository, and JwtService in TokenService

--- src/main/java/andrehsvictor/anitrace/token/TokenService.java
+++ src/main/java/andrehsvictor/anitrace/token/TokenService.java
@@ -2,10 +2,15 @@ package andrehsvictor.anitrace.token;
 
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.anitrace.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
 
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final RevokedTokenRepository revokedTokenRepository;
+    private final JwtService jwtService;
+    
 }
