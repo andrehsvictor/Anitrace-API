7ff2f56e16dc69fa042645aff49c4ede690cd4dd
andrehsvictor <andrehsvictor@gmail.com>
2025-01-13 20:34:36 -0300

    feat: add method to find user by username or email in UserRepository

--- src/main/java/andrehsvictor/anitrace/user/UserRepository.java
+++ src/main/java/andrehsvictor/anitrace/user/UserRepository.java
@@ -1,9 +1,14 @@
 package andrehsvictor.anitrace.user;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    @Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email")
+    Optional<User> findByUsernameOrEmail(String usernameOrEmail);
+    
 }
