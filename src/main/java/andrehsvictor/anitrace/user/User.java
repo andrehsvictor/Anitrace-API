08fb19b068499fcd41eb3213afa85e5ac8ca59b4
andrehsvictor <andrehsvictor@gmail.com>
2025-01-13 20:34:28 -0300

    feat: enhance User entity with additional fields and timestamps

--- src/main/java/andrehsvictor/anitrace/user/User.java
+++ src/main/java/andrehsvictor/anitrace/user/User.java
@@ -1,12 +1,59 @@
 package andrehsvictor.anitrace.user;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+@EqualsAndHashCode(of = { "id" })
+@ToString(exclude = { "password" })
 public class User implements Serializable {
 
     private static final long serialVersionUID = 7482934613802534053L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    private String displayName;
+    private String email;
+    private boolean emailVerified;
+    private String username;
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private UserProvider provider = UserProvider.LOCAL;
+
+    private String avatarUrl;
+
+    @Column(name = "password_hash")
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt = LocalDateTime.now();
 
 }
