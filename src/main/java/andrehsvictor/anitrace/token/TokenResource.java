2d337134546a54b29029715d45da5619ed275bb7
andrehsvictor <andrehsvictor@gmail.com>
2025-01-14 07:31:26 -0300

    feat: implement TokenResource and TokenService for token management and authentication

---
+++ src/main/java/andrehsvictor/anitrace/token/TokenResource.java
@@ -0,0 +1,36 @@
+package andrehsvictor.anitrace.token;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import andrehsvictor.anitrace.authentication.dto.AuthenticationDto;
+import andrehsvictor.anitrace.token.dto.AccessTokenDto;
+import andrehsvictor.anitrace.token.dto.TokenDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class TokenResource {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/v1/token")
+    public AccessTokenDto request(@Valid @RequestBody AuthenticationDto authenticationDto) {
+        return tokenService.request(authenticationDto);
+    }
+
+    @PostMapping("/api/v1/token/refresh")
+    public AccessTokenDto refresh(@Valid @RequestBody TokenDto tokenDto) {
+        return tokenService.refresh(tokenDto);
+    }
+
+    @DeleteMapping("/api/v1/token")
+    public ResponseEntity<Void> revoke(@Valid @RequestBody TokenDto tokenDto) {
+        tokenService.revoke(tokenDto);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/andrehsvictor/anitrace/token/TokenService.java
+++ src/main/java/andrehsvictor/anitrace/token/TokenService.java
@@ -1,8 +1,16 @@
 package andrehsvictor.anitrace.token;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.anitrace.authentication.AuthenticationService;
+import andrehsvictor.anitrace.authentication.dto.AuthenticationDto;
 import andrehsvictor.anitrace.jwt.JwtService;
+import andrehsvictor.anitrace.security.impl.UserDetailsImpl;
+import andrehsvictor.anitrace.token.dto.AccessTokenDto;
+import andrehsvictor.anitrace.token.dto.TokenDto;
+import andrehsvictor.anitrace.user.User;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,5 +20,37 @@ public class TokenService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final RevokedTokenRepository revokedTokenRepository;
     private final JwtService jwtService;
-    
+    private final AuthenticationService authenticationService;
+
+    public AccessTokenDto request(AuthenticationDto authenticationDto) {
+        Authentication authentication = authenticationService.authenticate(authenticationDto);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        User user = userDetails.getUser();
+        String subject = user.getId().toString();
+        return accessTokenDto(subject);
+    }
+
+    public AccessTokenDto refresh(TokenDto tokenDto) {
+        Jwt refreshToken = jwtService.decode(tokenDto.getToken());
+        refreshTokenRepository.deleteById(refreshToken.getId());
+        return accessTokenDto(refreshToken.getSubject());
+    }
+
+    public void revoke(TokenDto tokenDto) {
+        Jwt accessToken = jwtService.decode(tokenDto.getToken());
+        revokedTokenRepository.save(accessToken);
+    }
+
+    private AccessTokenDto accessTokenDto(String subject) {
+        Jwt accessToken = jwtService.issueAccessToken(subject);
+        Jwt refreshToken = jwtService.issueRefreshToken(subject);
+        refreshTokenRepository.save(refreshToken);
+        Long expiresIn = accessToken.getExpiresAt().getEpochSecond() - accessToken.getIssuedAt().getEpochSecond();
+        return AccessTokenDto.builder()
+                .accessToken(accessToken.getTokenValue())
+                .refreshToken(refreshToken.getTokenValue())
+                .expiresIn(expiresIn)
+                .build();
+    }
+
 }
