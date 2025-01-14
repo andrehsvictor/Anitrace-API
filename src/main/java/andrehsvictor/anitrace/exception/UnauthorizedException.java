a8d0ea9a42d667758b229453bc196f61556ca1e6
andrehsvictor <andrehsvictor@gmail.com>
2025-01-14 07:31:09 -0300

    feat: add UnauthorizedException with HTTP 401 status response

--- src/main/java/andrehsvictor/anitrace/exception/UnauthorizedException.java
+++ src/main/java/andrehsvictor/anitrace/exception/UnauthorizedException.java
@@ -1,7 +1,10 @@
 package andrehsvictor.anitrace.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends AuthenticationException {
 
     private static final long serialVersionUID = -1034258835771123099L;
