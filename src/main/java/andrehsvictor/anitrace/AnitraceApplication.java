6e373087ddd89602e2f19c099f86609cb249043f
andrehsvictor <andrehsvictor@gmail.com>
2025-01-14 09:23:25 -0300

    feat: add functionality to generate RSA key pair on application startup

--- src/main/java/andrehsvictor/anitrace/AnitraceApplication.java
+++ src/main/java/andrehsvictor/anitrace/AnitraceApplication.java
@@ -1,13 +1,37 @@
 package andrehsvictor.anitrace;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnitraceApplication {
 
-	public static void main(String[] args) {
+	private static String[] getPermissionCommand = {
+			"chmod", "+x", "generate_rsa_key_pair.sh"
+	};
+
+	private static String[] generateKeyPairCommand = {
+			"./generate_rsa_key_pair.sh"
+	};
+
+	public static void main(String[] args) throws IOException {
+		getGetPermission();
+		generateRsaKeyPair();
 		SpringApplication.run(AnitraceApplication.class, args);
+	}
+
+	private static void generateRsaKeyPair() throws IOException {
+		ProcessBuilder generateKeyPairProcessBuilder = new ProcessBuilder();
+		generateKeyPairProcessBuilder.command(generateKeyPairCommand);
+		generateKeyPairProcessBuilder.start();
+	}
+
+	private static void getGetPermission() throws IOException {
+		ProcessBuilder getPermissionProcessBuilder = new ProcessBuilder();
+		getPermissionProcessBuilder.command(getPermissionCommand);
+		getPermissionProcessBuilder.start();
 	}
 
 }
