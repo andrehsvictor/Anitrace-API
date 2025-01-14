9fb2b785b406d6a8ddf477b5df6c6d92888886f3
andrehsvictor <andrehsvictor@gmail.com>
2025-01-14 08:32:44 -0300

    feat: enhance UserService with user management methods and authentication retrieval

--- src/main/java/andrehsvictor/anitrace/user/UserService.java
+++ src/main/java/andrehsvictor/anitrace/user/UserService.java
@@ -2,8 +2,12 @@ package andrehsvictor.anitrace.user;
 
 import java.util.UUID;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.anitrace.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -11,8 +15,26 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final SecurityContext securityContext;
 
     public boolean existsById(UUID id) {
         return userRepository.existsById(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public UUID getAuthenticatedUserUuid() {
+        Jwt jwt = ((JwtAuthenticationToken) securityContext.getAuthentication()).getToken();
+        return UUID.fromString(jwt.getSubject());
+    }
+
+    public User getById(UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(User.class, "ID", id));
+    }
+
+    public void deleteById(UUID id) {
+        userRepository.deleteById(id);
     }
 }
