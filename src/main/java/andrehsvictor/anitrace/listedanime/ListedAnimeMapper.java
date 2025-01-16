bc4e3fb01e3785e74d238b7357a8228b4bd27781
andrehsvictor <andrehsvictor@gmail.com>
2025-01-16 14:10:52 -0300

    feat: add ListedAnimeMapper to convert ListedAnime to ListedAnimeDto using AnimeService

---
+++ src/main/java/andrehsvictor/anitrace/listedanime/ListedAnimeMapper.java
@@ -0,0 +1,21 @@
+package andrehsvictor.anitrace.listedanime;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import andrehsvictor.anitrace.anime.AnimeService;
+import andrehsvictor.anitrace.list.ListMapper;
+import andrehsvictor.anitrace.listedanime.dto.ListedAnimeDto;
+
+@Mapper(componentModel = "spring", uses = { ListMapper.class }, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public abstract class ListedAnimeMapper {
+
+    @Autowired
+    protected AnimeService animeService;
+
+    @Mapping(target = "anime", expression = "java(animeService.getById(listedAnime.getAnimeId()))")
+    public abstract ListedAnimeDto listedAnimeToListedAnimeDto(ListedAnime listedAnime);
+
+}
