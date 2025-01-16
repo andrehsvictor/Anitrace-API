7d91f6ec8c8cba42a210abc6d19522e0351cea2a
andrehsvictor <andrehsvictor@gmail.com>
2025-01-16 14:11:02 -0300

    feat: add pagination methods to ListedAnimeRepository for fetching anime by list ID and visibility

--- src/main/java/andrehsvictor/anitrace/listedanime/ListedAnimeRepository.java
+++ src/main/java/andrehsvictor/anitrace/listedanime/ListedAnimeRepository.java
@@ -2,8 +2,16 @@ package andrehsvictor.anitrace.listedanime;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import andrehsvictor.anitrace.list.ListVisibility;
 
 public interface ListedAnimeRepository extends JpaRepository<ListedAnime, UUID> {
 
+    Page<ListedAnime> findAllByListIdAndListVisibility(UUID listId, ListVisibility visibility, Pageable pageable);
+
+    Page<ListedAnime> findAllByListId(UUID listId, Pageable pageable);
+    
 }
