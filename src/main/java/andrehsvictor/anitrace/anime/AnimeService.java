cb6177ade69e2324bc0ba3198ee573c634bca863
andrehsvictor <andrehsvictor@gmail.com>
2025-01-16 14:06:00 -0300

    refactor: rename parameter in getById method in AnimeService interface

--- src/main/java/andrehsvictor/anitrace/anime/AnimeService.java
+++ src/main/java/andrehsvictor/anitrace/anime/AnimeService.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Pageable;
 import andrehsvictor.anitrace.anime.dto.AnimeDto;
 
 public interface AnimeService {
+
     Page<AnimeDto> getAll(Pageable pageable);
 
-    AnimeDto getById(Integer animeId);
+    AnimeDto getById(Integer id);
+
 }
