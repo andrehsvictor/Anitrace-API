d373db154681abb4b987902ce3eecc9f48417f07
andrehsvictor <andrehsvictor@gmail.com>
2025-01-16 14:05:49 -0300

    feat: add endpoint to retrieve anime by ID in AnimeResource

--- src/main/java/andrehsvictor/anitrace/anime/AnimeResource.java
+++ src/main/java/andrehsvictor/anitrace/anime/AnimeResource.java
@@ -3,6 +3,7 @@ package andrehsvictor.anitrace.anime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import andrehsvictor.anitrace.anime.dto.AnimeDto;
@@ -17,5 +18,10 @@ public class AnimeResource {
     @GetMapping("/api/v1/animes")
     public Page<AnimeDto> getAll(Pageable pageable) {
         return animeService.getAll(pageable);
+    }
+
+    @GetMapping("/api/v1/animes/{id}")
+    public AnimeDto getById(@PathVariable Integer id) {
+        return animeService.getById(id);
     }
 }
